227f380f53fec69bdc1c2f3999492ce467bde2b4
JITHENTHIRIYA <jithenthiriyack@gmail.com>
2026-03-09 14:20:06 -0700

    Vite Config bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/your-repo-name/" : "/",
+  base: command === "build" ? "/personalPortfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
